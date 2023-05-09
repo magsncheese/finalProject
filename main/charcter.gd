c7d84e2fa6c893ac577477f007b374b2224800d1
Yaren Dogan <yarddoga@vols.utk.edu>
2023-05-08 23:52:08 -0400

    fixed character movement

--- main/charcter.gd
+++ main/charcter.gd
@@ -1,16 +1,11 @@
 extends KinematicBody2D #like #include <.h file>
-
-export var move_speed : float = 300 #lets u change the move speed in teh catChar menu
+export var move_speed : float = 100 #lets u change the move speed in teh catChar menu
 export var starting_direction : Vector2 = Vector2(0, 1)
-
-# blend position: parameters/Idle/blend_position
 onready var animation_tree = $AnimationTree
-onready var state_machine = animation_tree.get("parameters/playback")
+
 
 var velocity := Vector2.ZERO
 
-func _ready():
-	update_animation_parameters(starting_direction)
 #------------------------------------------------------------------------------------------------#
 
 func _physics_process(_delta):
@@ -20,8 +15,6 @@ func _physics_process(_delta):
 		Input.get_action_strength("down") - Input.get_action_strength("up")  #positive value = go down (yaxis)
 	)
 		
-	
-	update_animation_parameters(input_direction)
 
 	#update velocity
 	velocity = input_direction * move_speed
@@ -32,21 +25,19 @@ func _physics_process(_delta):
 	pick_new_state()
 #------------------------------------------------------------------------------------------------#
 
-#i think this is where the bug is for the animation for walking
-func update_animation_parameters(move_input : Vector2):
-	#if the char is walking
-	if move_input != Vector2.ZERO:
-		#use the animation tree in the player sceen to play the animation
-		#for walking
-		animation_tree.set("parameters/walk_down/blend_position", move_input)
-		#for idling
-		animation_tree.set("parameters/Idle/blend_position", move_input)
-#------------------------------------------------------------------------------------------------#
-
 #cow go MOO
 func pick_new_state():
 	if velocity != Vector2.ZERO:
-		state_machine.travel("walk_down")
+		if abs(velocity.y) > abs(velocity.x):
+			if(velocity.y > 0):
+				$AnimationPlayer.play("walk_down")
+			else:
+				$AnimationPlayer.play("walk_up")
+		if abs(velocity.x) > abs(velocity.y):
+			if(velocity.x > 0):
+				$AnimationPlayer.play("walk_right")
+			else:
+				$AnimationPlayer.play("walk_left")
 	else:
-		state_machine.travel("idle_down")
+		$AnimationPlayer.play("idle_down")
 #------------------------------------------------------------------------------------------------#
