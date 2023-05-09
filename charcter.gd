e5a46aa02f6f65ae384003c2437466f05c170aa6
dotwan <124117298+dotwan@users.noreply.github.com>
2023-05-09 00:54:14 -0400

    item stacking
    trying to stack items in inventory

--- charcter.gd
+++ charcter.gd
@@ -1,4 +1,5 @@
 extends KinematicBody2D #like #include <.h file>
+<<<<<<< Updated upstream
 export var move_speed : float = 100 #lets u change the move speed in teh catChar menu
 export var starting_direction : Vector2 = Vector2(0, 1)
 onready var animation_tree = $AnimationTree
@@ -6,6 +7,20 @@ onready var animation_tree = $AnimationTree
 
 var velocity := Vector2.ZERO
 
+=======
+
+export var move_speed : float = 300 #lets u change the move speed in teh catChar menu
+export var starting_direction : Vector2 = Vector2(0, 1)
+
+# blend position: parameters/Idle/blend_position
+onready var animation_tree = $AnimationTree
+onready var state_machine = animation_tree.get("parameters/playback")
+
+var velocity := Vector2.ZERO
+
+func _ready():
+	update_animation_parameters(starting_direction)
+>>>>>>> Stashed changes
 #------------------------------------------------------------------------------------------------#
 
 func _physics_process(_delta):
@@ -14,7 +29,12 @@ func _physics_process(_delta):
 		Input.get_action_strength("right") - Input.get_action_strength("left"),  #positive value = go right (xaxis)
 		Input.get_action_strength("down") - Input.get_action_strength("up")  #positive value = go down (yaxis)
 	)
+<<<<<<< Updated upstream
 		
+=======
+
+	update_animation_parameters(input_direction)
+>>>>>>> Stashed changes
 
 	#update velocity
 	velocity = input_direction * move_speed
@@ -25,6 +45,7 @@ func _physics_process(_delta):
 	pick_new_state()
 #------------------------------------------------------------------------------------------------#
 
+<<<<<<< Updated upstream
 #cow go MOO
 func pick_new_state():
 	if velocity != Vector2.ZERO:
@@ -40,3 +61,23 @@ func pick_new_state():
 				$AnimationPlayer.play("walk_left")
 	else:
 		$AnimationPlayer.play("idle_down")
+=======
+#i think this is where the bug is for the animation for walking
+func update_animation_parameters(move_input : Vector2):
+	#if the char is walking
+	if move_input != Vector2.ZERO:
+		#use the animation tree in the player sceen to play the animation
+		#for walking
+		animation_tree.set("parameters/walk_down/blend_position", move_input)
+		#for idling
+		animation_tree.set("parameters/Idle/blend_position", move_input)
+#------------------------------------------------------------------------------------------------#
+
+#cow go MOO
+func pick_new_state():
+	if velocity != Vector2.ZERO:
+		state_machine.travel("walk_down")
+	else:
+		state_machine.travel("idle_down")
+#------------------------------------------------------------------------------------------------#
+>>>>>>> Stashed changes
